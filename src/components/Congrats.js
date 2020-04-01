70b47a5635a19f07f9e4091964a4fad7c587aef7
AntonSynytsia <anton.synytsia@gmail.com>
2020-04-01 11:56:03 -0700

    Initial setup, test utils, and congrats component with tests

---
+++ src/components/Congrats.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Functional React component for congratulatory message.
+ * @function
+ * @param {JSX.Element} - Rendered component (or null if `success` prop)
+ */
+const Congrats = ({ success }) => {
+  return (
+    <div data-test="component-congrats">
+      {success && (
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      )}
+    </div>
+  );
+};
+
+Congrats.propTypes = {};
+
+export default Congrats;
